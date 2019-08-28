dde967adc4446566c07bebe553ad07c44f323f39
gamze <gamzeezmag@users.noreply.github.com>
2019-08-28 16:25:49 +0300

    Initial Commit

---
+++ Yonca/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yonca
+//
+//  Created by Gamze Çalışkan on 28.08.2019.
+//  Copyright © 2019 Gamze Çalışkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
